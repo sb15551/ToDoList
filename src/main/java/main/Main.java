22691d245caaca83cdd5918474900c3854d1f2b3
Aleksey Surkov <46666503+sb15551@users.noreply.github.com>
2020-09-23 10:33:24 +0400

    Added string 'Start'

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Start");
+        
         SpringApplication.run(Main.class, args);
     }
 }
